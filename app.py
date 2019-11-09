39c934d04703cddc82b51c21339f5ac034f9d1be
junwoo77lee <47956198+junwoo77lee@users.noreply.github.com>
2019-11-09 10:39:19 -0700

    created some app routes and connection to the database, and modified on the dropdown menu.

--- app.py
+++ app.py
@@ -1,9 +1,37 @@
-from flask import Flask, jsonify, render_template
+import pandas as pd
 
+from flask import Flask, jsonify, render_template
+from sqlalchemy import create_engine
+from flask_sqlalchemy import SQLAlchemy
+
+# Create an app
 app = Flask(__name__)
 
+# Set-up Databases
+db_name = 'shotcharts.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = f'sqlite:///static/db/{db_name}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/shotchart/<player_id>", methods=['GET'])
+def get_shotchart_for_player(player_id):
+    query = f"""
+        SELECT *
+         FROM shotcharts sc
+         WHERE sc.PLAYER_ID = {player_id}
+         AND sc.GAME_DATE LIKE '2019%'
+    """
+    df = pd.read_sql(query, con=db.session.bind)
+    json = eval(df[['LOC_X', 'LOC_Y', 'EVENT_TYPE']].to_json(
+        orient='table', index=False))['data']
+    return jsonify(json)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
